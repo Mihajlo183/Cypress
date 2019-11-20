4d5fef9098408dc02f420a918965669d03c32e1d
Mihajlo183 <miko183@gmail.com>
2019-11-20 20:22:39 +0100

    dodadt fajl index

---
+++ cypress/utils/index.js
@@ -0,0 +1,9 @@
+// Random email function is the function for random email
+
+export const randomEmail = () => {
+    return (
+        Math.random()
+            .toString(36)
+            .substr(2,7) + '@vivifyacademy.com'
+    );
+}
