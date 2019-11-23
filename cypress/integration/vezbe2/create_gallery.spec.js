35f65e0fa60f38de9dc1c04fb467c73f245a08fe
Mihajlo183 <miko183@gmail.com>
2019-11-23 13:54:00 +0100

    ...

--- cypress/integration/vezbe2/create_gallery.spec.js
+++ cypress/integration/vezbe2/create_gallery.spec.js
@@ -1,6 +1,7 @@
 import { EMAIL } from '../../fixtures/constants'
 import { authPage } from '../../page_object/login.page'
 import { createGallery } from '../../page_object/create_gallery.page'
+import { randomEmail } from '../../utils'
 
 
 describe('My First Test', function(){
@@ -31,12 +32,24 @@ describe('My First Test', function(){
     })
 
     it('TC - 03 Create gallery - positive case', function() {
+        var RANDOM = randomEmail()
         cy.contains('Create Gallery').click()
-        createGallery.title.type('This is the title')
+        createGallery.title.type(RANDOM)
         createGallery.description.type('This is a validd description')
         createGallery.picture.type('https://www.espreso.rs/data/images/2017/12/08/15/298425_atila-sabo-cigota_ff.jpg')
         cy.contains('Submit').click()
+        // cy.get('.box-title').eq(0).should('have.text', '\n This is the title\n')
+        cy.get('.box-title').eq(0).should('contain', RANDOM)
 
+
+    })
+
+    it.only('TC - 03 Create gallery - input field', function(){
+        cy.contains('Create Gallery').click()
+        cy.contains('Add image').click()
+        createGallery.picture.eq(1).should('exist')
+        cy.get('i[class="fas fa-trash"]').eq(0).click()
+        createGallery.picture.eq(1).should('not.exist')        
     })
 
     
