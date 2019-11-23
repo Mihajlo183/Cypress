368509b5bf49cb1b26b2afc715939e12ce9bc0a0
Mihajlo183 <miko183@gmail.com>
2019-11-23 12:07:26 +0100

    vezbe2 - prva stavka

--- cypress/integration/vezbe2/create_gallery.spec.js
+++ cypress/integration/vezbe2/create_gallery.spec.js
@@ -6,14 +6,29 @@ describe('My First Test', function(){
     
     beforeEach(() => {
         cy.visit('/login')
-        // cy.contains('Login').click()
+
         authPage.login(EMAIL.EXISTING, EMAIL.PASSWORD)
-        cy.contains('Submit').click()
+        
         
     })
 
-    it('TC - 01 My gallery page - pagination', function(){
+    // it('TC - 01 My gallery page - pagination', function(){
+    //     cy.get('.cell').eq(9).should('exist')
+    //     cy.get('.cell').eq(10).should('not.exist')
+    //     cy.get('button[class="btn btn-custom"]').click()
+    //     //Nikad ne koristi eksplicitan wait u miliskeundama kao ispod
+    //     cy.wait(4000)
+    //     cy.get('.cell').eq(19).should('exist')
+    //     cy.get('.cell').eq(20).should('not.exist')
 
+    // })
+    // DRUGI NACIN
+    it('TC - 02 My gallery page - pagination', function(){
+        cy.get('div.grid').children().should('have.length', 10)
+        cy.contains('Load More').click()
+        cy.get('div.grid').children().should('not.have.length',10)
     })
+
+    
 
 })
