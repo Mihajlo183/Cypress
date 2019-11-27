a00d502475594abb002be6af07bb5150ac373c19
Mihajlo183 <miko183@gmail.com>
2019-11-27 22:09:43 +0100

    stubovanje

--- cypress/integration/vezbe3/pagination.spec.js
+++ cypress/integration/vezbe3/pagination.spec.js
@@ -3,28 +3,36 @@ describe('My First Test', function(){
     
     beforeEach(() => {
         cy.login(EMAIL.EXISTING, EMAIL.PASSWORD)
-        // cy.visit('/')
-
+        // 
         
     })
 
     it('TC - 01 My gallery page - pagination', function(){
         cy.server()
-        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery.json').as('stub')
+        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery_count9.json').as('stub')
+        cy.visit('/')
+        cy.get('.cell').eq(8).should('exist')
         cy.get('.btn-custom').should('not.exist')
     })
 
 
     it('TC - 02 My gallery page - pagination', function(){
         cy.server()
-        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery.json').as('stub')
+        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery_count10.json').as('stub')
+        cy.visit('/')
+        cy.get('.cell').eq(9).should('exist')
         cy.get('.btn-custom').should('not.exist')
     })
 
-    it('TC - 03 My gallery page - pagination', function(){
+    it.only('TC - 03 My gallery page - pagination', function(){
         cy.server()
-        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery.json').as('stub')
+        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=1&term=', 'fixture:gallery_count11.json').as('stub')
+        cy.visit('/')
         cy.get('.btn-custom').should('exist')
+        cy.route('GET', Cypress.config('backendUrl') + 'galleries?page=2&term=', 'fixture:gallery_count1.json').as('stub1')
+        cy.get('.btn-custom').click()
+        cy.get('.cell').eq(10).should('exist')
+        cy.get('.btn-custom').should('not.exist')
     })
 
 })
